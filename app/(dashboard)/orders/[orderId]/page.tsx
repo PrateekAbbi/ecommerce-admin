70c5f13045c2d1600034c39ca62fafc226110cdb
prateekabbi <arjunprateek31@gmail.com>
2025-01-15 22:23:43 -0500

    Updated OrdersID page

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -2,11 +2,17 @@ import { DataTable } from "@/components/custom ui/DataTable";
 import { columns } from "@/components/orderItems/OrderItemsColumns";
 import { Separator } from "@/components/ui/separator";
 
-const OrderDetails = async ({ params }: { params: { orderId: string }}) => {
-  const res = await fetch(`${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`)
-  const { orderDetails, customer } = await res.json()
+type tParams = Promise<{ orderId: string }>;
 
-  const { street, city, state, postalCode, country } = orderDetails.shippingAddress
+const OrderDetails = async ({ params }: { params: tParams }) => {
+  const { orderId } = await params;
+  const res = await fetch(
+    `${process.env.ADMIN_DASHBOARD_URL}/api/orders/${orderId}`
+  );
+  const { orderDetails, customer } = await res.json();
+
+  const { street, city, state, postalCode, country } =
+    orderDetails.shippingAddress;
 
   return (
     <div className="flex flex-col p-10 gap-5">
