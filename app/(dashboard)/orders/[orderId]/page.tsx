33860370fb7977a709a3a5eb25d0c844f4a62dc8
prateekabbi <arjunprateek31@gmail.com>
2025-01-15 22:15:54 -0500

    Updated OrderId erro

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -2,16 +2,11 @@ import { DataTable } from "@/components/custom ui/DataTable";
 import { columns } from "@/components/orderItems/OrderItemsColumns";
 import { Separator } from "@/components/ui/separator";
 
-const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
-  const { orderId } = await params;
+const OrderDetails = async ({ params }: { params: { orderId: string }}) => {
+  const res = await fetch(`${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`)
+  const { orderDetails, customer } = await res.json()
 
-  const res = await fetch(
-    `${process.env.ADMIN_DASHBOARD_URL}/api/orders/${orderId}`
-  );
-  const { orderDetails, customer } = await res.json();
-
-  const { street, city, state, postalCode, country } =
-    orderDetails.shippingAddress;
+  const { street, city, state, postalCode, country } = orderDetails.shippingAddress
 
   return (
     <div className="flex flex-col p-10 gap-5">
